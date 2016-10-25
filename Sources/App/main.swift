5d14daa5e09f9c178b245b839e706715e5fa4220
Michał Cichoń <michal.cichon@codete.com>
2016-10-25 20:25:56 +0200

    simple routing test

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -3,7 +3,15 @@ import Vapor
 let drop = Droplet()
 
 drop.get { request in
-    return "Hello"
+    return try JSON(node: [
+        "message" : "Hello"
+        ]);
+}
+
+drop.get("test") { request in
+    return try JSON(node: [
+        "message" : "Test"
+        ]);
 }
 
 drop.resource("posts", PostController())
