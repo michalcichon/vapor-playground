e08a9f4cea3037600c221ad3847162e15bdc161c
Michał Cichoń <michal.cichon@codete.com>
2016-10-25 20:13:24 +0200

    hello

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -2,10 +2,8 @@ import Vapor
 
 let drop = Droplet()
 
-drop.get { req in
-    return try drop.view.make("welcome", [
-    	"message": drop.localization[req.lang, "welcome", "title"]
-    ])
+drop.get { request in
+    return "Hello"
 }
 
 drop.resource("posts", PostController())
